1217d2c46f9755769808901a72cfe3406dd07a9f
Bonian Chen <bonianchen@google.com>
2022-05-01 10:13:01 +0800

    [Settings] Stop listening data connected intent for presenting reset APN dialog
    Suspening the monitering of intent(TelephonyIntents.ACTION_ANY_DATA_CONNECTION_STATE_CHANGED)
    during the stage of resetting default APN, since its more like a noice
    once the reset progress stared (since user pressed the menu button).
    
    Bug: 190127964
    Test: local
    Change-Id: I2536c12f333848a780d70b8478610e8fb6d7951b
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>

--- src/com/android/settings/network/apn/ApnSettings.java
+++ src/com/android/settings/network/apn/ApnSettings.java
@@ -451,6 +451,9 @@ public class ApnSettings extends RestrictedSettingsFragment
     }
 
     private boolean restoreDefaultApn() {
+        // Callback of data connection change could be some noise during the stage of restore.
+        mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+
         showDialog(DIALOG_RESTORE_DEFAULTAPN);
         mRestoreDefaultApnMode = true;
 
@@ -501,6 +504,7 @@ public class ApnSettings extends RestrictedSettingsFragment
                         getResources().getString(
                                 R.string.restore_default_apn_completed),
                         Toast.LENGTH_LONG).show();
+                    restartPhoneStateListener(mSubId);
                     break;
             }
         }
