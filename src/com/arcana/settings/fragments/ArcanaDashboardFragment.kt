projectarcana-aosp/platform_packages_apps_Settings

Settings: Import BasePreferenceController and DashboardFragment from Krypton
Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

src/com/arcana/settings/fragments/ArcanaDashboardFragment.kt
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.arcana.settings.fragment
+
+import androidx.preference.Preference
+
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.dashboard.DashboardFragment
+
+abstract class ArcanaDashboardFragment: DashboardFragment() {
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.ARCANA
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        super.onDisplayPreferenceDialog(preference)
+    }
+
+    companion object {
+        const val REQUEST_KEY = "ArcanaDashboardFragment#RequestKey"
+    }
+}

SpatiaDroid/android_packages_apps_Settings

src/com/arcana/settings/fragments/ArcanaDashboardFragment.kt
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.arcana.settings.fragment
+
+import androidx.preference.Preference
+
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.dashboard.DashboardFragment
+
+abstract class ArcanaDashboardFragment: DashboardFragment() {
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.CRDROID_SETTINGS
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        super.onDisplayPreferenceDialog(preference)
+    }
+
+    companion object {
+        const val REQUEST_KEY = "ArcanaDashboardFragment#RequestKey"
+    }
+}