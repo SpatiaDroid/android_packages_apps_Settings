72a8bee05ab813f22936d60230318eb9635460ba
minarypenguin <alexfinhart@gmail.com>
2022-07-08 05:10:52 +0800

    Laboratory: Replace skiavk with skiagl
    * for stability across mtk and qcom devices
    
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>

--- src/com/android/settings/laboratory/SkiaglPreferenceController.java
+++ src/com/android/settings/laboratory/SkiaglPreferenceController.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2022 Project Kaleidoscope
+ * Copyright (C) 2022 riceDroid
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -20,18 +21,17 @@ import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.R;
 import com.android.settingslib.development.SystemPropPoker;
 
-public class SkiavkPreferenceController extends TogglePreferenceController {
+public class SkiaglPreferenceController extends TogglePreferenceController {
 
     private static final String PROPERTY_RENDERER = "persist.sys.hwui.renderer";
     private static final String PROPERTY_RENDERER_ENGINE = "persist.sys.renderengine.backend";
     private static final String PROPERTY_SKIA_OPS = "persist.sys.renderthread.skia.reduceopstasksplitting";
     private static final String OPENGL_THREADED = "threaded";
     private static final String SKIA_THREADED = "skiaglthreaded";
-    private static final String PROPERTY_USES_VULKAN = "ro.hwui.use_vulkan";
     private static final String OPENGL = "opengl";
-    private static final String SKIAVK = "skiavk";
+    private static final String SKIAGL = "skiagl";
 
-    public SkiavkPreferenceController(Context context, String key) {
+    public SkiaglPreferenceController(Context context, String key) {
         super(context, key);
     }
 
@@ -43,15 +43,15 @@ public class SkiavkPreferenceController extends TogglePreferenceController {
     @Override
     public boolean isChecked() {
         String renderer = SystemProperties.get(PROPERTY_RENDERER);
-        if ("".equals(renderer))
-            return "true".equals(SystemProperties.get(PROPERTY_USES_VULKAN));
+        if ("skiagl".equals(renderer))
+            return true;
         else
-            return SKIAVK.equals(renderer);
+            return SKIAGL.equals(renderer);
     }
 
     @Override
     public boolean setChecked(boolean isChecked) {
-        SystemProperties.set(PROPERTY_RENDERER, isChecked ? SKIAVK : OPENGL);
+        SystemProperties.set(PROPERTY_RENDERER, isChecked ? SKIAGL : OPENGL);
         SystemProperties.set(PROPERTY_RENDERER_ENGINE, isChecked ? SKIA_THREADED : OPENGL_THREADED);
         SystemProperties.set(PROPERTY_SKIA_OPS, isChecked ? "true" : "false");
         SystemPropPoker.getInstance().poke();
